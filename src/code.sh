37cff050c1749e4258c9ee873620f3688d145d7c
addy81 <adriana.tou@gmail.com>
2021-08-05 16:06:17 +0100

    added workflow failedState step with samplesheet log expansion

--- src/code.sh
+++ src/code.sh
@@ -3,8 +3,6 @@
 # Output each line as it is executed (-x) and stop if any non zero exit codes are seen (-e)
 set -exo pipefail
 
-mark-section "download inputs"
-
 mkdir -p /home/dnanexus/runfolder TSO500_ruo out/logs/logs out/analysis_folder
 
 # download all inputs
@@ -13,27 +11,26 @@ dx-download-all-inputs --parallel --except input_files
 #Unload and prep docker image
 unzip $TSO500_ruo_path -d TSO500_ruo 
 rm $TSO500_ruo_path
+
 # change the owner of the app
 chmod 777 TSO500_ruo/TSO500_RUO_LocalApp/
+
 # move the docker image into ~
 mv TSO500_ruo/TSO500_RUO_LocalApp/trusight-oncology-500-ruo-dockerimage-ruo-*.tar /home/dnanexus/
+
 # load docker image
 sudo docker load --input /home/dnanexus/trusight-oncology-500-ruo-dockerimage-ruo-*.tar
 
 options=""
 
-if [ "$samplesheet" ]; then options+=" --sampleSheet $samplesheet_path"; fi
-
+# Analysis starting from FastQ files
 if [ "$isFastQ" = true ];
 then
+    echo "This analysis is starting from FastQ inputs"
     options+=" --fastqFolder /home/dnanexus/runfolder"
 
-    if [ ! "$samplesheet" ]
-    then 
-        dx-jobutil-report-error "Please provide a SampleSheet for analysis. :)" 
-        exit 1
-    fi
-    # Download all fastq files from the array
+    # Download all FastQ files from the array
+    echo "Downloading FastQ inputs"
     for i in "${!input_files[@]}"
         do 
             dx download "${input_files[$i]}" -o runfolder/
@@ -60,8 +57,11 @@ then
     cd /home/dnanexus
 
 else
+    # If the analysis input is not FastQs expect tar archive(s) of bcl files
     options+=" --runFolder /home/dnanexus/runfolder/" 
+
     # download the runfolder input, decompress and save in directory 'runfolder'
+    echo " Downloading tar archive(s)"
     for i in "${!input_files_name[@]}" 
     do
         # check format and decompress
@@ -81,6 +81,22 @@ else
 
 fi
 
+
+#Find the SampleSheet if provided as an input or present in the input directory
+if [ "$samplesheet" ];
+then 
+    echo "SampleSheet was provided as input ${samplesheet_name}"
+    mv -v /home/dnanexus/in/samplesheet/*.csv /home/dnanexus/runfolder/SampleSheet.csv
+
+elif [ -f /home/dnanexus/runfolder/*heet.csv ];
+then
+    echo "SampleSheet is present in the run directory"
+    mv -v /home/dnanexus/runfolder/*heet.csv /home/dnanexus/runfolder/SampleSheet.csv 
+else
+    dx-jobutil-report-error "SampleSheet not provided or found, please check your inputs."
+    exit 1
+fi
+
 # Adds additional non-specified optional arguments to the command
 if [ "$analysis_options" ]; then options+=" ${analysis_options}"; fi 
 if [ "$isNovaSeq" = true ]; then options+=" --isNovaSeq"; fi
@@ -88,13 +104,24 @@ if [ "$isNovaSeq" = true ]; then options+=" --isNovaSeq"; fi
 # run the shell script, specifying the analysis folder, runfolder, samplesheet, resourcesFolder and any analysis options string given as an input
 # pipe stderr into stdout and write this to a file and to screen - this allows a record of the logs to be saved and visible on screen if it goes wrong
 
-echo sudo bash TSO500_ruo/TSO500_RUO_LocalApp/TruSight_Oncology_500_RUO.sh --analysisFolder /home/dnanexus/out/analysis_folder/analysis_folder --resourcesFolder /home/dnanexus/TSO500_ruo/TSO500_RUO_LocalApp/resources "${options[@]}"
+echo "Starting analysis"
 
 sudo bash TSO500_ruo/TSO500_RUO_LocalApp/TruSight_Oncology_500_RUO.sh \
 --analysisFolder /home/dnanexus/out/analysis_folder/analysis_folder \
 --resourcesFolder /home/dnanexus/TSO500_ruo/TSO500_RUO_LocalApp/resources \
 ${options} 2>&1 | tee /home/dnanexus/out/logs/logs/RUO_stdout.txt
 
+if less RUO_stdout.txt | grep WorkflowSucceededState ; 
+then 
+    echo "LocalApp Completed Successfully";
+else 
+    echo "Local App Failed"
+    echo "SampleSheet Validation log:"
+    less /home/dnanexus/out/analysis_folder/analysis_folder/Logs_Intermediates/SamplesheetValidation/SamplesheetValidation-*.log
+
+    dx-jobutil-report-error "ERROR: Workflow failed to complete"
+    exit 1
+fi
 
 # upload all outputs
 dx-upload-all-outputs --parallel
